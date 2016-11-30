a2a444a0b21960e109538a7c0f041f7865e31841
lycaste <yaoha403@126.com>
2016-11-30 16:58:07 +0800

    112
    123

--- wechat.py
+++ wechat.py
@@ -1,30 +1,25 @@
-# from flask import Flask, request, make_response
-# from config import Config
-# import hashlib
-# app = Flask(__name__)
-#
-# @app.route('/', methods=['POST','GET'])
-# def wechat():
-# 	if request.method == 'GET':
-# 		print("receive Get")
-# 		data = request.args
-# 		toekn = Config.WECHAT_TOKEN
-# 		signature = data.get('signature','')
-# 		timestamp = data.get('timestamp','')
-# 		nonce = data.get('nonce','')
-# 		echostr = data.get('echostr','')
-# 		s = [timestamp, nonce, toekn]
-# 		s.sort()
-# 		s = ''.join(s)
-# 		if (hashlib.sha1(s.encode('utf-8')).hexdigest() == signature):
-# 			make_response(echostr)
-#
-#
-# if __name__ == '__main__':
-# 	app.run(host='0.0.0.0', port=80)
-#
+from flask import Flask, request, make_response
+from config import Config
+import hashlib
+app = Flask(__name__)
 
-import werobot
+@app.route('/', methods=['POST','GET'])
+def wechat():
+	if request.method == 'GET':
+		print("receive Get")
+		data = request.args
+		toekn = Config.WECHAT_TOKEN
+		signature = data.get('signature','')
+		timestamp = data.get('timestamp','')
+		nonce = data.get('nonce','')
+		echostr = data.get('echostr','')
+		s = [timestamp, nonce, toekn]
+		s.sort()
+		s = ''.join(s)
+		if (hashlib.sha1(s.encode('utf-8')).hexdigest() == signature):
+			make_response(echostr)
 
-robot = werobot.WeRoBot(token='wechat_token')
-robot.run(host='0.0.0.0',port=80)
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=80)
+
